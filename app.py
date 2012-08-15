c2d0de1096c490ad46f9df04653916f36b0a35c6
graeme <graeme@graeme-desktop.(none)>
2012-08-14 20:00:09 -0500

    Stubbing out the functions for milestone 1 of spidersilk.  Also, added python_modules so we can see what's what.
    Yes, I know, I need to fix my vim config.

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def hello():
-	return "Hello world"
+	return "This will be the front page of SpiderSilk"
+
+
+@app.route("/story/<int:story_id>")
+def story(story_id):
+	return "You have selected story %s" % story_id
+
+@app.route("/user/<user_name>")
+def user(user_name):
+	return "You have selected user %s " % user_name
+
 
 if __name__ == '__main__':
 	app.run()
