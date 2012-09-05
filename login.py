37e96bce264231654c14e0b729c5b9ec794b482a
Jarrod Henry <jarrodhenry@gmail.com>
2012-09-04 21:26:05 -0500

    finishing up some look and feel

--- login.py
+++ login.py
@@ -6,17 +6,23 @@ def process_login_form():
 	password = request.form['password']
 	text = "Hello %s, your password is %s" % (login,password)
 	user = dbsession.query(User).filter_by(name=login).first()
+	if password == None:
+		flash("Please enter a password")
+		return redirect(url_for('login'))
+
+	if user == None:
+		flash("Please enter a username")
+		return redirect(url_for('login'))
 	
 	if password == user.password:
 		session['username'] = request.form['login']
-		flash("Login Successful")	
 	else: 
 		flash("Invalid login")
+		return redirect(url_for('login'))
+
 
 	return redirect(url_for('hello')) 
 
 def display_login_form():
-	flash ("User Logged Out")
-
 	return render_template('login.html')
 
