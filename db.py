a2d634adb7a2d891b044431c86abd0ba4759586d
Jarrod Henry <jarrodhenry@gmail.com>
2012-10-06 11:42:54 -0500

    committing table for user follows

--- db.py
+++ db.py
@@ -41,6 +41,12 @@ tagstory_association_table = Table('tagstory', Base.metadata,
 	Column('story_id', Integer, ForeignKey('stories.id'))
 )
 
+userfollowing_table = Table('following', Base.metadata,
+	Column('user', Integer, ForeignKey('users.id')),
+	Column('following', Integer, ForeignKey('users.id'))
+)
+
+
 class Story(Base):
 	""" Story Class Model """
 	__tablename__ = 'stories'
