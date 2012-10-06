a16e9c11a0d949bd0f43c443ddaedb98937523a0
Jarrod Henry <jarrodhenry@gmail.com>
2012-10-06 15:16:10 -0500

    Adding tag generation to automatic data generator.

--- seeddatabase.py
+++ seeddatabase.py
@@ -15,9 +15,12 @@
 if __name__ == '__main__':
 	password = '$2a$12$/xOeN.4B6wQsen5TgvOAMOUVvETJ8vDC8WqhjHRF2OYGSslxMmB2O'
 
-	from db import User, Story, addDefault, refresh_db, session as dbsession
+	from db import User, Story, Tag, addDefault, refresh_db, session as dbsession
 	import random
 	import loremipsum
+
+	refresh_db()
+ 	addDefault()
 
 	for adduser in range(1,1000):
 		username = "User%s" % (adduser)
@@ -34,9 +37,23 @@ if __name__ == '__main__':
 		newstory = Story(loremipsum.generate_sentence()[2])
 		newstory.text = loremipsum.generate_paragraph()[2]
 		newstory.adult = True
-		newstory.uid = random.randrange(1000)+1
+		newstory.uid = random.randrange(999)+1
 		dbsession.add(newstory)
 		dbsession.commit()
+
+	for tagnumber in range(1,100):
+		tag = Tag("tag"+str(tagnumber))
+		dbsession.add(tag)
+		dbsession.commit()
+
+	for stories in range(1,20000):
+		story = dbsession.query(Story).filter_by(id = stories).first()
+		for newtagid in range(0,5):
+			tag = dbsession.query(Tag).filter_by(id = random.randrange(99)).first()
+			story.tags.append(tag)
+			dbsession.commit()
+
+	
 
 
 
