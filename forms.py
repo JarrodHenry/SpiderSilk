a7473305c6f518330987f2e367855d1929042983
Jarrod Henry <jarrodhenry@gmail.com>
2012-10-27 12:45:57 -0500

    Required fields

--- forms.py
+++ forms.py
@@ -6,15 +6,15 @@ class LoginForm(Form):
 
 class RegistrationForm(Form):
 	''' Let's do a basic registration form for now '''
-	username = TextField('Username', [Required()])
-	password = PasswordField('Password', [Required(), EqualTo('confirm', message='Passwords must match')])
-	confirm = PasswordField('Repeat Password')
+	username = TextField('Username*', [Required()])
+	password = PasswordField('Password*', [Required(), EqualTo('confirm', message='Passwords must match')])
+	confirm = PasswordField('Repeat Password*')
 	species = TextField('Species')
 	gender = TextField('Gender') 
-	email = TextField('Email address', [Required(), validators.Email()])
+	email = TextField('Email address*', [Required(), validators.Email()])
 	bio = TextAreaField('Bio')
 	adult = BooleanField('I am an adult.')
-	accepttos = BooleanField('I accept the terms of service.', [validators.Required( message='You must accept the terms of service')])
+	accepttos = BooleanField('I accept the terms of service.*', [validators.Required( message='You must accept the terms of service')])
 
 class StoryForm(Form):
 	''' This will allow users to add stories '''
