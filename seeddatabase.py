edcc4f2dfab4961483041ab1439d033098e9a15b
Jarrod Henry <jarrodhenry@gmail.com>
2013-01-10 19:58:29 -0600

    story template changes, seeddatabase

--- seeddatabase.py
+++ seeddatabase.py
@@ -15,9 +15,10 @@
 if __name__ == '__main__':
 	password = '$2a$12$/xOeN.4B6wQsen5TgvOAMOUVvETJ8vDC8WqhjHRF2OYGSslxMmB2O'
 
-	from db import User, Story, Tag, addDefault, refresh_db, session as dbsession
+	from db import User, Story, Tag, Reccomendation, addDefault, refresh_db, session as dbsession
 	import random
 	import loremipsum
+	from datetime import datetime
 
 	refresh_db()
  	addDefault()
@@ -67,6 +68,30 @@ if __name__ == '__main__':
 		if stories % 500 == 0: 
 			print "Story " + str(stories) + " tagged."
 
+	print "Adding faves"
+	for user in range(1,1000):
+		u = dbsession.query(User).filter_by(id = user).first()
+		for newfave in range(0, random.randrange(30)):
+			s = dbsession.query(Story).filter_by(id=random.randrange(20000)).first()
+			u.faves.append(s)
+			
+		dbsession.commit()	
+		if user % 100 == 0:
+			print "User " + str(user) + " faves set."
+
+	
+	print "Adding between zero and thirty reccomendations to stories"
+	for stories in range(1,20000):
+		story = dbsession.query(Story).filter_by(id=stories).first()
+		for newrec in range(0, random.randrange(30)):
+			rec = Reccomendation("this is a good story")
+			rec.uid = random.randrange(99)+1 
+			u = dbsession.query(User).filter_by(id = rec.uid).first()
+			rec.uname = u.name
+			rec.sid = story.id
+			rec.date = datetime.now()
+			story.recs.append(rec)
+		dbsession.commit()
 
 	
 
